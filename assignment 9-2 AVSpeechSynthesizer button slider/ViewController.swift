c19039aeb0f0695515966799cf67ca338b86c8ac
Ryan CY <phillin820109@msn.com>
2022-08-30 09:23:22 +0800

    Initial Commit

---
+++ assignment 9-2 AVSpeechSynthesizer button slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment 9-2 AVSpeechSynthesizer button slider
+//
+//  Created by Ryan Lin on 2022/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
